5a7bb9f10384cf8ac7552eda1168d0d14ff1ce65
pttu1 <oh.willy@hotmail.com>
2022-05-22 23:42:55 -0400

    installed docker engine on jenkins server

--- cicd-terraform/backend-s3.tf
+++ cicd-terraform/backend-s3.tf
@@ -1,18 +1,18 @@
 #CREATE S3 REMOTE STORAGE FOR STATE FILE
 terraform {
   backend "s3" {
-      bucket = "vprofile-kops-k8s"
-      region = "us-east-1"
-      dynamodb_table = "tf-state-locking"
-      encrypt = true
+    bucket         = "vprofile-kops-k8s"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-locking"
+    encrypt        = true
   }
 }
 
 #CREATE LOCKING MECHANISM WITH DYNAMODB
 resource "aws_dynamodb_table" "tf-locks" {
-  name = "tf-state-locking"
+  name         = "tf-state-locking"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
--- cicd-terraform/vars.tf
+++ cicd-terraform/vars.tf
@@ -1,22 +1,26 @@
-variable AWS_REGION {
-    default =  "us-east-2"
+variable "AWS_REGION" {
+  default = "us-east-2"
 }
 
-variable AMI {
-    type = map
-    default = {
-        us-east-2 = "ami-03a0c45ebc70f98ea"
-    }
+variable "AMI" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-081c75eaeac28ac34"
+  }
 }
 
-variable PRIVATE_KEY_PATH {
-    default = "keys/devopskey"
+variable "PRIVATE_KEY_PATH" {
+  default = "keys/devopskey"
 }
 
-variable PUBLIC_KEY_PATH {
-    default = "keys/devopskey.pub"
+variable "PUBLIC_KEY_PATH" {
+  default = "keys/devopskey.pub"
 }
 
-variable MY-IP {
+variable "MY-IP" {
   default = "104.158.234.69/32"
+}
+
+variable "instance_count" {
+  default = 1
 }
